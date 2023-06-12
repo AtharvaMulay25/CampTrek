fd801991f175e95ee409f921772b049ccc45e751
nishad-dhuri-05 <113404885+nishad-dhuri-05@users.noreply.github.com>
2023-06-12 17:41:08 +0530

    Added utilities and views folder, schemas and json files

---
+++ schemas.js
@@ -0,0 +1,28 @@
+//These are just validation schemas using Joi and not model schemas
+const Joi = require('joi');
+const review = require('./models/review');
+
+module.exports.campgroundSchema = Joi.object(
+    {
+            campground: Joi.object(
+            {
+                title: Joi.string().required(),
+                price: Joi.number().required().min(0),
+                description: Joi.string().required(),
+                location: Joi.string().required(),
+                image: Joi.string().required()
+            }
+        ).required()
+    }
+);
+
+module.exports.reviewSchema = Joi.object(
+    {
+        review: Joi.object(
+            {
+                body: Joi.string().required(),
+                rating: Joi.number().required().min(1).max(5)
+            }
+        ).required()
+    }
+)
